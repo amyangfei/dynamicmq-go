ff8728e998d576f35e8ddb64c597d9a655ae2300
amyangfei <amyangfei@gmail.com>
2015-04-05 23:32:28 +0800

    Add hash split by percent.

--- chord/vnode.go
+++ chord/vnode.go
@@ -21,3 +21,27 @@ func HashJump(start []byte, step, maxhash *big.Int) []byte {
 	}
 	return bi.Bytes()
 }
+
+func HashSplit(start, end []byte, percent int) []byte {
+	if percent < 1 || percent > 99 {
+		percent = 50
+	}
+
+	biStart := big.NewInt(0)
+	biStart.SetBytes(start)
+	biEnd := big.NewInt(0)
+	biEnd.SetBytes(end)
+	biSplit := big.NewInt(0)
+
+	if percent == 50 {
+		biSplit.Sub(biEnd, biStart)
+		biSplit.Div(biSplit, big.NewInt(2))
+		biSplit.Add(biStart, biSplit)
+	} else {
+		biSplit.Sub(biEnd, biStart)
+		biSplit.Mul(biSplit, big.NewInt(int64(percent)))
+		biSplit.Div(biSplit, big.NewInt(100))
+		biSplit.Add(biStart, biSplit)
+	}
+	return biSplit.Bytes()
+}
--- chord/vnode_test.go
+++ chord/vnode_test.go
@@ -52,6 +52,42 @@ func TestHashJump(t *testing.T) {
 	}
 }
 
+func TestHashSplit(t *testing.T) {
+	start := big.NewInt(0)
+	end := big.NewInt(0)
+	end.Div(maxhash, big.NewInt(int64(vnodeNum)))
+
+	bi := big.NewInt(0)
+	bi.SetBytes(HashSplit(start.Bytes(), end.Bytes(), 1))
+	if bi.String() != "913438523331814323877303020447676887284957839" {
+		t.Errorf("error value for hash split by 1%%")
+	}
+	bi.SetBytes(HashSplit(start.Bytes(), end.Bytes(), 2))
+	if bi.String() != "1826877046663628647754606040895353774569915678" {
+		t.Errorf("error value for hash split by 2%%")
+	}
+	bi.SetBytes(HashSplit(start.Bytes(), end.Bytes(), 3))
+	if bi.String() != "2740315569995442971631909061343030661854873518" {
+		t.Errorf("error value for hash split by 3%%")
+	}
+	bi.SetBytes(HashSplit(start.Bytes(), end.Bytes(), 10))
+	if bi.String() != "9134385233318143238773030204476768872849578393" {
+		t.Errorf("error value for hash split by 10%%")
+	}
+	bi.SetBytes(HashSplit(start.Bytes(), end.Bytes(), 25))
+	if bi.String() != "22835963083295358096932575511191922182123945984" {
+		t.Errorf("error value for hash split by 25%%")
+	}
+	bi.SetBytes(HashSplit(start.Bytes(), end.Bytes(), 50))
+	if bi.String() != "45671926166590716193865151022383844364247891968" {
+		t.Errorf("error value for hash split by 50%%")
+	}
+	bi.SetBytes(HashSplit(start.Bytes(), end.Bytes(), 75))
+	if bi.String() != "68507889249886074290797726533575766546371837952" {
+		t.Errorf("error value for hash split by 75%%")
+	}
+}
+
 func BenchmarkHashJump(b *testing.B) {
 	start := make([]byte, hashBytes)
 	start[hashBytes-1] = 1
